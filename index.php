332e05d77b3a1794086f60cf83a67457d2df9d13
Satyams-git <satyam.du.in@gmail.com>
2025-09-21 20:57:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -124,7 +124,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>DevOps Batch 17 Notes App </h1>
+        <h1>DevOps Batch 17 Notes App with CI/CD Automation </h1>
         <form action="save.php" method="post">
             <textarea name="note" placeholder="Write your DevOps notes here..."></textarea><br>
             <button type="submit">💾 Save Note</button>
