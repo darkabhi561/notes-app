a56c68cbb2cb48792f8b400fc4c4b287a3630b96
darkabhi561 <72434603+darkabhi561@users.noreply.github.com>
2025-09-23 23:05:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DevOps Batch 17th Notes App</title>
+    <title>DevOps Batch 17 Noted App</title>
     <style>
         body {
             font-family: "Segoe UI", Arial, sans-serif;
